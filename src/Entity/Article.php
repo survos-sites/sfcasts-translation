e593f390fecbd8c6a676bc890917806dc1370f0c
Tac Tacelosky <tacman@gmail.com>
2025-08-24 21:54:09 -0400

    integrate babel-bundle and translator-bundle

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -1,120 +1,155 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Entity;
 
-use App\Entity\Translations\ArticleTranslationsTrait;
-use Survos\BabelBundle\Attribute\BabelStorage;
-use Survos\BabelBundle\Attribute\StorageMode;
-use Survos\BabelBundle\Entity\Traits\TranslatableHooksTrait;
-use Survos\BabelBundle\Contract\TranslatableResolvedInterface;
 use App\Repository\ArticleRepository;
+use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Survos\BabelBundle\Attribute\BabelStorage;
+use Survos\BabelBundle\Attribute\StorageMode;
 use Survos\BabelBundle\Attribute\Translatable;
-/**
- * @property string|null $title [translatable via *TranslationsTrait]
- */
+use Survos\BabelBundle\Contract\TranslatableResolvedInterface;
+use Survos\BabelBundle\Entity\Traits\TranslatableHooksTrait;
+
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 #[BabelStorage(StorageMode::Property)]
 class Article implements TranslatableResolvedInterface
 {
-    use TranslatableHooksTrait;
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
+
     #[ORM\Column(length: 255)]
     private ?string $slug = null;
+
     #[ORM\Column]
-    private ?\DateTimeImmutable $publishedAt = null;
+    private ?DateTimeImmutable $publishedAt = null;
+
     #[ORM\Column(length: 255)]
     private ?string $author = null;
+
     #[ORM\ManyToOne(inversedBy: 'articles')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Category $category = null;
-    /**
-     * @var Collection<int, Tag>
-     */
 
-    #[Translatable()]
-    #[ORM\Column(length: 255)]
-    public ?string $title = null;
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    protected ?string $titleBacking = null;
 
-    #[Translatable()]
-    #[ORM\Column(type: Types::TEXT, length: 255)]
-    public $content = null;
+    #[Translatable]
+    public ?string $title {
+        set {
+            $this->titleBacking = $value;
+        }
+        get {
+            return $this->resolveTranslatable('title', $this->titleBacking, 'title');
+        }
+    }
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    protected ?string $contentBacking = null;
+
+    #[Translatable]
+    public ?string $content {
+        set {
+            $this->contentBacking = $value;
+        }
+        get {
+            return $this->resolveTranslatable('content', $this->contentBacking, 'content');
+        }
+    }
+
     #[ORM\ManyToMany(targetEntity: Tag::class, mappedBy: 'articles')]
     private Collection $tags;
-    public function __construct()
-    {
-        $this->tags = new ArrayCollection();
+
+use TranslatableHooksTrait;
+
+public function __construct()
+{
+    $this->tags = new ArrayCollection();
+}
+
+public function getId(): ?int
+{
+    return $this->id;
+}
+
+public function getSlug(): ?string
+{
+    return $this->slug;
+}
+
+public function setSlug(string $slug): static
+{
+    $this->slug = $slug;
+    return $this;
+}
+
+public function getTitle(): ?string
+{
+    return $this->title;
+}
+
+public function getPublishedAt(): ?\DateTimeImmutable
+{
+    return $this->publishedAt;
+}
+
+public function setPublishedAt(\DateTimeImmutable $publishedAt): static
+{
+    $this->publishedAt = $publishedAt;
+    return $this;
+}
+
+public function getAuthor(): ?string
+{
+    return $this->author;
+}
+
+public function setAuthor(string $author): static
+{
+    $this->author = $author;
+    return $this;
+}
+
+public function getCategory(): ?Category
+{
+    return $this->category;
+}
+
+public function setCategory(?Category $category): static
+{
+    $this->category = $category;
+    return $this;
+}
+
+/**
+ * @return Collection<int, Tag>
+ */
+public function getTags(): Collection
+{
+    return $this->tags;
+}
+
+public function addTag(Tag $tag): static
+{
+    if (!$this->tags->contains($tag)) {
+        $this->tags->add($tag);
+        $tag->addArticle($this);
     }
-    public function getId(): ?int
-    {
-        return $this->id;
+    return $this;
+}
+
+public function removeTag(Tag $tag): static
+{
+    if ($this->tags->removeElement($tag)) {
+        $tag->removeArticle($this);
     }
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
-    public function setSlug(string $slug): static
-    {
-        $this->slug = $slug;
-        return $this;
-    }
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
-    public function getPublishedAt(): ?\DateTimeImmutable
-    {
-        return $this->publishedAt;
-    }
-    public function setPublishedAt(\DateTimeImmutable $publishedAt): static
-    {
-        $this->publishedAt = $publishedAt;
-        return $this;
-    }
-    public function getAuthor(): ?string
-    {
-        return $this->author;
-    }
-    public function setAuthor(string $author): static
-    {
-        $this->author = $author;
-        return $this;
-    }
-    public function getCategory(): ?Category
-    {
-        return $this->category;
-    }
-    public function setCategory(?Category $category): static
-    {
-        $this->category = $category;
-        return $this;
-    }
-    /**
-     * @return Collection<int, Tag>
-     */
-    public function getTags(): Collection
-    {
-        return $this->tags;
-    }
-    public function addTag(Tag $tag): static
-    {
-        if (!$this->tags->contains($tag)) {
-            $this->tags->add($tag);
-            $tag->addArticle($this);
-        }
-        return $this;
-    }
-    public function removeTag(Tag $tag): static
-    {
-        if ($this->tags->removeElement($tag)) {
-            $tag->removeArticle($this);
-        }
-        return $this;
-    }
+    return $this;
+}
 }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -1,85 +1,92 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Entity;
 
-use Survos\BabelBundle\Attribute\BabelStorage;
-use Survos\BabelBundle\Entity\Traits\TranslatableHooksTrait;
-use Survos\BabelBundle\Contract\TranslatableResolvedInterface;
 use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Survos\BabelBundle\Attribute\BabelStorage;
 use Survos\BabelBundle\Attribute\Translatable;
+use Survos\BabelBundle\Contract\TranslatableResolvedInterface;
+use Survos\BabelBundle\Entity\Traits\TranslatableHooksTrait;
 
-/**
- * @property string|null $name [translatable via *TranslationsTrait]
- */
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
-#[BabelStorage()]
+#[\BabelStorage]
 class Category implements TranslatableResolvedInterface
 {
-    use TranslatableHooksTrait;
-
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    protected ?string $nameBacking = null;
+
     #[Translatable]
-    public ?string $name = null;
-    /**
-     * @var Collection<int, Article>
-     */
+    public ?string $name {
+        set {
+            $this->nameBacking = $value;
+        }
+        get {
+            return $this->resolveTranslatable('name', $this->nameBacking, 'name');
+        }
+    }
+
     #[ORM\OneToMany(targetEntity: Article::class, mappedBy: 'category', orphanRemoval: true)]
     private Collection $articles;
 
-    public function __construct()
-    {
-        $this->articles = new ArrayCollection();
-    }
+use TranslatableHooksTrait;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+public function __construct()
+{
+    $this->articles = new ArrayCollection();
+}
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
+public function getId(): ?int
+{
+    return $this->id;
+}
 
-    public function setName(string $name): static
-    {
-        $this->name = $name;
-        return $this;
-    }
+public function getName(): ?string
+{
+    return $this->name;
+}
 
-    /**
-     * @return Collection<int, Article>
-     */
-    public function getArticles(): Collection
-    {
-        return $this->articles;
-    }
+public function setName(string $name): static
+{
+    $this->name = $name;
+    return $this;
+}
 
-    public function addArticle(Article $article): static
-    {
-        if (!$this->articles->contains($article)) {
-            $this->articles->add($article);
-            $article->setCategory($this);
+/**
+ * @return Collection<int, Article>
+ */
+public function getArticles(): Collection
+{
+    return $this->articles;
+}
+
+public function addArticle(Article $article): static
+{
+    if (!$this->articles->contains($article)) {
+        $this->articles->add($article);
+        $article->setCategory($this);
+    }
+    return $this;
+}
+
+public function removeArticle(Article $article): static
+{
+    if ($this->articles->removeElement($article)) {
+        // set the owning side to null (unless already changed)
+        if ($article->getCategory() === $this) {
+            $article->setCategory(null);
         }
-        return $this;
     }
-
-    public function removeArticle(Article $article): static
-    {
-        if ($this->articles->removeElement($article)) {
-            // set the owning side to null (unless already changed)
-            if ($article->getCategory() === $this) {
-                $article->setCategory(null);
-            }
-        }
-        return $this;
-    }
+    return $this;
+}
 }
